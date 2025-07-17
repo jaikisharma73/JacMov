9949b7e75982acc9a67c03c83cd28d2b3517ca27
jaikisharma73 <jack912062sharmas@gmail.com>
2025-07-17 17:32:03 +0530

    now

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -11,7 +11,7 @@ const Movies = () => {
     <div className='relative my-30 mb-60 px-6 md:px-16 lg:px-16 xl:px-18 overflow-hidden min-h-[80vh]'>
       <BlurCircle top="50px" left="0px" />
       <BlurCircle bottom="20px" right="10px" />
-      <h1 className='text-lg font-medium my-4'>Now Showing</h1>
+      <h1 className='text-lg font-medium my-4'>Now Showing.</h1>
       <div className='flex flex-wrap max-sm:justify-center gap-8'>
         {shows.map((movie)=>(
           <MovieCard movie = {movie} key={movie._id} />
