eeb0906c33bee4d09a56230ac824ffcb4f292de2
jaikisharma73 <jack912062sharmas@gmail.com>
2025-07-17 15:39:54 +0530

    added booking

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
 import showRouter from './routes/showRoutes.js';
-import bookingRouter from './routes/bookingROutes.js';
+import bookingRouter from './routes/bookingRoutes.js';
 import adminRouter from './routes/adminRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import { stripeWebhooks } from './controllers/stripeWebhook.js';
